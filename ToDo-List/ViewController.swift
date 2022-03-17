9c69ef1ef9439fab6c72698defa8c80ba9cd4a24
Selin Ilya <IVSelin@mail.ru>
2022-03-17 15:19:14 +0400

    Initial Commit

---
+++ ToDo-List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo-List
+//
+//  Created by Ilya Selin on 17.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
